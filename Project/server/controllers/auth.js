5a41e54bc224f64ba8f9783f65e15e013ad2764b
Aadit Harshal Baldha <70932466+whitewizard2002@users.noreply.github.com>
2023-04-12 11:04:58 +0530

    Update auth.js

--- Project/server/controllers/auth.js
+++ Project/server/controllers/auth.js
@@ -38,6 +38,33 @@ export const register = async (req, res) => {
   }
 };
 
+/* LOGGIN IN FOR ADMIN */
+
+export const adminLogin = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email: email });
+    if (!user) return res.status(400).json({ msg: "Admin does not exist. " });
+
+    if(password==user.password)
+      var isMatch=true;
+    else
+      var isMatch=false;
+    
+    // const isMatch = await compare(password, user.password);
+    if (!isMatch) return res.status(400).json({ msg: "Invalid credentials. " });
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    delete user.password;
+    // console.log(token)
+    res.status(200).json({ token, user });
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).json({ error: err.message });
+  }
+};
+
 /* LOGGING IN */
 export const login = async (req, res) => {
   try {
