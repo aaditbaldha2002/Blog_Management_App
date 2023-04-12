e062366ea72322324ec5a064eca648fbdc4b5195
Aadit Harshal Baldha <70932466+whitewizard2002@users.noreply.github.com>
2023-04-12 11:03:10 +0530

    Update admin.js

--- Project/server/controllers/admin.js
+++ Project/server/controllers/admin.js
@@ -1,14 +1,22 @@
 import Post from "../models/Post.js";
 import {spawn} from 'child_process';
+
 /** For Getting Post Analysis */
-posts=await Post.find();
-export const getPostStats=(req,res)=>{
-    const childPython=spawn('python',['sentimentAnalysis.py',JSON.stringify({...posts})]);
+export const getPostStats=async (req,res)=>{
+    try {
+        const {postId}=req.params;
+        var id=postId;
+        console.log("Data sent to python file:"+JSON.stringify(id));
+    } catch (err) {
+        res.status(404).json({ message: err.message });
+        console.log("error finding the post");
+    }
+    const childPython=spawn('python',['./controllers/sentimentAnalysis.py',JSON.stringify(id)]);
     childPython.stdout.on('data',(data)=>{
-        console.log(`Output of dataPreprocessing.py file: ${data}`)
+        console.log("Output of python file:"+ data);
+        res.status(200).json({msg:`${data}`});
     });
     childPython.stderr.on('data',(err)=>{
         console.log(`Error of python file: ${err}`);
     });
 };
-  
