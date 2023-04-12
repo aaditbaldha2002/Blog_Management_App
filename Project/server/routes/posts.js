c5968c8ca3deedc12af1ae948acb12b0e3ad512f
Aadit Harshal Baldha <70932466+whitewizard2002@users.noreply.github.com>
2023-04-12 11:12:24 +0530

    Update posts.js

--- Project/server/routes/posts.js
+++ Project/server/routes/posts.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getFeedPosts, getSearchedPosts, getUserPosts, likePost , commentPost, deletePost /*getPostComments*/} from "../controllers/posts.js";
+import { getFeedPosts, getUserPosts, likePost , commentPost, getUserPost, getSearchedPosts,deletePost} from "../controllers/posts.js";
 import { verifyToken } from "../middleware/auth.js";
 
 const router = express.Router();
@@ -7,16 +7,16 @@ const router = express.Router();
 /* READ */
 router.get("/", verifyToken, getFeedPosts);
 router.get("/:searchquery", verifyToken, getSearchedPosts);
-
 router.get("/:userId/posts", verifyToken, getUserPosts);
+router.get("/:postId",verifyToken,getUserPost);
 // router.get("/:id/showcomments",verifyToken,getPostComments);
 
 /* UPDATE */
 router.patch("/:id/like", verifyToken, likePost);
 router.patch("/:id/comment",verifyToken,commentPost);
 
-
 /* DELETE */
 router.delete("/:id",verifyToken,deletePost);
 
 export default router;
+
