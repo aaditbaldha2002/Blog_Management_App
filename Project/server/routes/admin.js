1472a9cea79bdeca8532740aa656f23e6234d22a
Aadit Harshal Baldha <70932466+whitewizard2002@users.noreply.github.com>
2023-04-12 11:10:07 +0530

    Update admin.js

--- Project/server/routes/admin.js
+++ Project/server/routes/admin.js
@@ -1,14 +1,10 @@
 import express from "express";
-import { getPostStats } from "../controllers/admin.js";
+import { getPostStats} from "../controllers/admin.js";
 import { verifyToken } from "../middleware/auth.js";
 
 const router = express.Router();
 
 /* READ for post analysis*/
-router.get("/admin", verifyToken,getPostStats);
-
-
-/* UPDATE */
-// router.patch("/:id/:friendId", verifyToken, addRemoveFriend);
+router.get("/:postId/analysis",verifyToken,getPostStats);
 
 export default router;
